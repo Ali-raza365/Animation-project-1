5d51708e6bbdce6d448b89978c9f362bf194a4ba
Ali-raza365 <mianraza645@gmail.com>
2021-03-18 18:26:46 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import useWebAnimations from "@wellyshen/use-web-animations";
+import Earth from "./Components/Earth";
 
 function App() {
+  const { ref, playState, getAnimation } = useWebAnimations({
+    keyframes: {
+      transform: ["translateX(100%)", "translateX(-100%)"],
+    },
+    timing: {
+      duration: 36000,
+      iterations: Infinity,
+    },
+    onReady: ({ playState, animate, animation }) => {
+      // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+    },
+    onUpdate: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the running state or changes state
+    },
+    onFinish: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="wrapper">
+        <div className="sky" />
+        <Earth />
+        <div className="scenery" ref={ref} id="foreground1">
+          <img
+            id="palm3"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
+            alt=" "
+          />
+        </div>
+        <div className="scenery" id="foreground2">
+          <img
+            id="bush"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
+            alt=" "
+          />
+          <img
+            id="w_rook_upright"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
+            alt=" "
+          />
+        </div>
+        <div className="scenery" id="background1">
+          <img
+            id="r_pawn_upright"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x"
+            alt=" "
+          />
+          <img
+            id="w_rook"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
+            alt=" "
+          />
+          <img
+            id="palm1"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x"
+            alt=" "
+          />
+        </div>
+        <div className="scenery" id="background2">
+          <img
+            id="r_pawn"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x"
+            alt=" "
+          />
+          <img
+            id="r_knight"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x"
+            alt=" "
+          />
+          <img
+            id="palm2"
+            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png"
+            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"
+            alt=" "
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
