6706504bb039f101a0c6558634c09c1e84ac65cc
ALI Raza <mianraza645@gmail.com>
2021-03-22 22:59:38 +0500

    final project

--- src/App.js
+++ src/App.js
@@ -1,95 +1,121 @@
-import logo from "./logo.svg";
 import "./App.css";
-import useWebAnimations from "@wellyshen/use-web-animations";
 import Earth from "./Components/Earth";
+import Scenery from "./Components/Scenery";
+import useWebAnimations from "@wellyshen/use-web-animations";
+import { useEffect } from "react";
+
 
 function App() {
-  const { ref, playState, getAnimation } = useWebAnimations({
-    keyframes: {
-      transform: ["translateX(100%)", "translateX(-100%)"],
-    },
-    timing: {
-      duration: 36000,
-      iterations: Infinity,
-    },
-    onReady: ({ playState, animate, animation }) => {
-      // Triggered when the animation is ready to play (Google Chrome: available in v84+)
-    },
-    onUpdate: ({ playState, animate, animation }) => {
-      // Triggered when the animation enters the running state or changes state
-    },
-    onFinish: ({ playState, animate, animation }) => {
-      // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
-    },
+////////redQueen_alice animation //
+  var playbackrateRQ = 1;
+	var playbackrateBG = 0;
+
+const spriteFrames = [
+  { transform: 'translateY(0)' },
+  { transform: 'translateY(-100%)' }   
+];
+const spirteTiming = {
+  easing: "steps(7, end)",
+  direction: "reverse",
+  duration: 600,
+  playbackRate: playbackrateRQ,
+  iterations: Infinity,
+};
+const redQueen_alice = useWebAnimations({
+  keyframes: spriteFrames,
+  timing: spirteTiming,
+});
+
+//////// background foreground animation //////
+
+const sceneryFrames = [
+  { transform: "translateX(100%)" },
+  { transform: "translateX(-100%)" },
+];
+
+const sceneryTimingBackground = {
+  duration: 36000,
+  iterations: Infinity,
+  playbackRate: playbackrateBG,
+};
+const sceneryTimingForeground = {
+  duration: 12000,
+  iterations: Infinity,
+  playbackRate: playbackrateBG,
+};
+
+const background1 = useWebAnimations({
+  keyframes: sceneryFrames,
+  timing: sceneryTimingBackground,
+});
+const background2 = useWebAnimations({
+  keyframes: sceneryFrames,
+  timing: sceneryTimingBackground,
+});
+const foreground1 = useWebAnimations({
+  keyframes: sceneryFrames,
+  timing: sceneryTimingForeground,
+});
+const foreground2 = useWebAnimations({
+  keyframes: sceneryFrames,
+  timing: sceneryTimingForeground,
+});
+
+//////// adjustBackgroundPlayback animation /////
+
+const adjustBackgroundPlayback = () => {
+  if (playbackrateRQ < 0.8) {
+   let back = (playbackrateRQ / 2) * -1   
+    playbackrateBG = back  >0 ?back :0  ;
+  } else if (playbackrateRQ > 1.2) {
+    playbackrateBG = playbackrateRQ / 2;
+  } else  {
+    playbackrateBG = 0;
+  
+  }
+
+    foreground1.getAnimation().playbackRate = playbackrateBG;
+    foreground2.getAnimation().playbackRate = playbackrateBG;
+    background1.getAnimation().playbackRate = playbackrateBG;
+    background2.getAnimation().playbackRate = playbackrateBG;
+};
+
+useEffect(() => {
+  const fganimation = foreground1.getAnimation();
+  fganimation.currentTime = fganimation.effect.getTiming().duration / 2;
+
+
+  const bganimation = background1.getAnimation();
+  bganimation.currentTime = bganimation.effect.getTiming().duration / 2;
+
+  setInterval(() => {
+    /* Set decay */
+
+    if (playbackrateRQ > 0.4) {
+      playbackrateRQ *= 0.9;
+      redQueen_alice.getAnimation().playbackRate = playbackrateRQ;
+    }
+      adjustBackgroundPlayback();
+  }, 3000);
+
+  document.addEventListener("click", () => {
+    playbackrateRQ *= 1.4;
+    redQueen_alice.getAnimation().playbackRate = playbackrateRQ;
+    adjustBackgroundPlayback();
   });
+});
   return (
     <>
       <div className="wrapper">
         <div className="sky" />
-        <Earth />
-        <div className="scenery" ref={ref} id="foreground1">
-          <img
-            id="palm3"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
-            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3.png 2x"
-            alt=" "
-          />
-        </div>
-        <div className="scenery" id="foreground2">
-          <img
-            id="bush"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush_small.png"
-            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/bush.png 2x"
-            alt=" "
-          />
-          <img
-            id="w_rook_upright"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright_small.png"
-            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_upright.png 2x"
-            alt=" "
-          />
-        </div>
-        <div className="scenery" id="background1">
-          <img
-            id="r_pawn_upright"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
-            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright.png 2x"
-            alt=" "
-          />
-          <img
-            id="w_rook"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
-            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
-            alt=" "
-          />
-          <img
-            id="palm1"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
-            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1.png 2x"
-            alt=" "
-          />
-        </div>
-        <div className="scenery" id="background2">
-          <img
-            id="r_pawn"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_small.png"
-            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn.png 2x"
-            alt=" "
-          />
-          <img
-            id="r_knight"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight_small.png"
-            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_knight.png 2x"
-            alt=" "
-          />
-          <img
-            id="palm2"
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2_small.png"
-            srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm2.png 2x"
-            alt=" "
-          />
-        </div>
-      </div>
+        <Earth  Ref={redQueen_alice.ref}/>
+        <Scenery
+          background1={background1.ref}
+          background2={background2.ref}
+          foreground1={foreground1.ref}
+          foreground2={foreground2.ref}
+        />
+    </div>
     </>
   );
 }
--- src/Components/Earth.js
+++ src/Components/Earth.js
@@ -1,34 +1,14 @@
 import React from "react";
 import useWebAnimations from "@wellyshen/use-web-animations";
 
-export default function Sky() {
-  const { ref, playState, getAnimation } = useWebAnimations({
-    keyframes: {
-      transform: ["translateY(0)", "translateY(-100%)"],
-    },
-    timing: {
-      easing: "steps(7, end)",
-      direction: "reverse",
-      duration: 600,
-      playbackRate: 1,
-      iterations: Infinity,
-    },
-    onReady: ({ playState, animate, animation }) => {
-      // Triggered when the animation is ready to play (Google Chrome: available in v84+)
-    },
-    onUpdate: ({ playState, animate, animation }) => {
-      // Triggered when the animation enters the running state or changes state
-    },
-    onFinish: ({ playState, animate, animation }) => {
-      // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
-    },
-  });
+export default function Earth({Ref}) {
+  
   return (
     <>
       <div className="earth">
         <div id="red-queen_and_alice">
           <img
-            ref={ref}
+            ref={Ref}
             id="red-queen_and_alice_sprite"
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
             srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen.png 2x"
